38a730e9165a8ac6762a8f3b6010bc3a1cb6e280
miriamfx <miriamfx2@gmail.com>
2017-02-05 00:54:16 -0200

    Create oid.py

---
+++ oid.py
@@ -0,0 +1,10 @@
+from pysnmp.hlapi import *
+from os import *
+
+Descr = ('1.3.6.1.6.3.1.1.1')
+ObjectID = ('1.3.6.1.6.3.1.1.2')
+UpTime = ('1.3.6.1.6.3.1.1.3')
+Contact = ('1.3.6.1.6.3.1.1.4')
+Name = ('1.3.6.1.6.3.1.1.5')
+Location = ('1.3.6.1.6.3.1.1.6')
+Services = ('1.3.6.1.6.3.1.1.7')
