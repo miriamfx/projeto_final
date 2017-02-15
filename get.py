21727d688a67af9cc220f8967a1bf215fe592347
miriamfx <miriamfx2@gmail.com>
2017-02-14 23:07:58 -0200

    Update get.py

--- get.py
+++ get.py
@@ -1,33 +1,34 @@
 from pysnmp.hlapi import *
+from kivy.properties import ObjectProperty
 from os import *
-import index
 import oid
 
-ip = index.resp
 
-g = errorIndication, errorStatus, errorIndex, varBinds = next(
-    getCmd(SnmpEngine(),
-    CommunityData('public'),
-    UdpTransportTarget((ip, 161)),
-    ContextData(),
-    ObjectType(ObjectIdentity(oid.Descr)),
-    ObjectType(ObjectIdentity(oid.UpTime)),
-    ObjectType(ObjectIdentity(oid.Contact)),
-    ObjectType(ObjectIdentity(oid.Number)),
-    ObjectType(ObjectIdentity(oid.OperStatus)),
-    ObjectType(ObjectIdentity(oid.Forwarding)),
-    ObjectType(ObjectIdentity(oid.InReceives)),
-    ObjectType(ObjectIdentity(oid.CurrentEstab)),
+class SimpleSnmp():
+    def __init__(self, ip, community):
+        self.ip = ip
+        self.community = community
 
-               ),
 
-)
+    def GetSNMP(self):
+        resultado = errorIndication, errorStatus, errorIndex, varBinds = next(
+            getCmd(SnmpEngine(),
+                   CommunityData(self.community),
+                   UdpTransportTarget((self.ip, 161)),
+                   ContextData(),
+                   ObjectType(ObjectIdentity(oid.Descr)),
+                   ObjectType(ObjectIdentity(oid.UpTime)),
+                   ObjectType(ObjectIdentity(oid.Contact)),
+                   ObjectType(ObjectIdentity(oid.Name)),
+                   ObjectType(ObjectIdentity(oid.ObjectID)),
+                   ObjectType(ObjectIdentity(oid.Location)),
+                   ObjectType(ObjectIdentity(oid.Services)),
+                   ), )
+        if errorIndication:
+            return str(errorIndication)
+        elif errorStatus:
+            return str('%s at %s' % (errorStatus.prettyPrint(),
+                                     errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        else:
+            self.set_result_form (str(resultado))
 
-if errorIndication:
-    print(errorIndication)
-elif errorStatus:
-    print('%s at %s' % (errorStatus.prettyPrint(),
-                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-else:
-
-    print (g)
