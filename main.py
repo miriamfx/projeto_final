13432e36b4132705106c4fd6918c4f3255d91928
miriamfx <miriamfx2@gmail.com>
2017-02-17 20:23:24 -0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 import kivy
 from kivy.app import App
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
-from get import SimpleSnmp 
+from kivy.uix.popup import Popup
+from get import SimpleSnmp
 
 class SnmpToolApp(App):
-    h = '::. Get .::'
-    #textinput_resultado = ObjectProperty(None)
+    h = 'Consultar'
 
     def build(self):
         Window.size = (500, 250)
         self.load_kv('main.kv')
 
     def set_result_form(self, resultado):
-        #self.ids["textinput_resultado"].text = resultado
+        self.root.ids.textinput_resultado.text = resultado
         print (resultado)
 
-
     def get_values_form(self, ip, community):
-        a= SimpleSnmp(ip,community)
+        a = SimpleSnmp(ip, community)
         result = a.GetSNMP()
-       #result = result + ' para o IP ' + ip
-        #result = result + ' e Community ' + community
+        result = result + '\n IP ' + ip
+        result = result + ' e Community ' + community
         self.set_result_form(result)
 
 if __name__ == "__main__":
