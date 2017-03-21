043fb7d4446ebaf92613827d9a7f05c754d753ae
miriamfx <miriamfx2@gmail.com>
2017-03-20 23:21:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,30 @@ from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from get import SimpleSnmp
 
+
 class SnmpToolApp(App):
     h = 'Consultar'
 
     def build(self):
-        Window.size = (500, 250)
+        Window.size = (1000, 250)
         self.load_kv('main.kv')
 
     def set_result_form(self, resultado):
         self.root.ids.textinput_resultado.text = resultado
         print (resultado)
 
+
     def get_values_form(self, ip, community):
         a = SimpleSnmp(ip, community)
         result = a.GetSNMP()
         result = result + '\n IP ' + ip
         result = result + ' e Community ' + community
+
+
+
         self.set_result_form(result)
+
+
 
 if __name__ == "__main__":
     SnmpToolApp().run()
